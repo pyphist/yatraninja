b3dd4efc89eab5351f42a0b110e9c33cba5078c9
root <pyphist@outlook.com>
2019-09-17 15:10:34 -0700

    storage.py updated

--- core/storage.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class ProfilePicStorage(S3Boto3Storage):
-    location = settings.AWS_PROFILE_LOCATION
-    file_overwrite = False
-    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
